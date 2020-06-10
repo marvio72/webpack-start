a5a3a8b622d5849dcf3f75a611f14207adeffe84
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-06-10 17:06:56 -0500

    webpack-inicial

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import {
+  saludar
+} from './js/componentes.js';
+// import './styles.css';
+import './sass/estilos.scss';
+// import img from './assets/dcd5e077cf9f54ebe52d4f7ebe8c3080.png';
+
+const nombre = 'Marco 48';
+
+saludar(nombre);
